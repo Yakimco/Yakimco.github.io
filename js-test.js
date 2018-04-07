7ae6fabb2c668740890e784d20254f09c8ba55c6
Vadim <Yakim@users.noreply.github.com>
2018-04-07 16:27:15 +0300

    js2

--- js-test.js
+++ js-test.js
@@ -1,12 +1,33 @@
-window.onload = function () {
-    var button = document.getElementById('StartButton');
-    button.onclick = function () {
-        var firstNum = Number(prompt('Enter first number'));
-        var secondNum = Number(prompt('Enter second number'));
-        if (firstNum >= 0 && firstNum <= 10 && secondNum <= 10 && secondNum >= 0){
-            alert('Are you serious? You can do it by yourself.');
+'use strict';
+var answer;
+do{
+    answer = parseInt(prompt('1+1=?'));
+    if(answer != 2){
+        var b = confirm('wrong answer. try again');
+        if (b == false){
+            break;
         }
-        var sum = firstNum + secondNum;
-        alert('Result is ' + sum);
     }
+
+}
+while (answer !=2);
+if (answer == 2) {
+    var first_right_answer = confirm('you are right next question?');
+    if (first_right_answer == true) {
+        var factorial_num = Math.floor(Math.random() * 20 + 1);
+        var factorial_question = parseInt(prompt('f(' + factorial_num + ')=?'));
+
+        function factorial(n) {
+            return (n != 1) ? n * factorial(n - 1) : 1;
+        }
+
+        if (factorial(factorial_num) === factorial_question) {
+            alert('You are awesome');
+        }
+        else {
+            alert('You are wrong.');
+        }
+
+    }
+
 }
